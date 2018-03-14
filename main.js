8bbb835d847a9c284d84052727fd722df87ba89c
gcgallo <ggallo102@gmail.com>
2018-03-14 12:54:48 -0700

    webcam example refresh

--- main.js
+++ main.js
@@ -14,10 +14,13 @@ function init() {
     renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
     container = document.getElementById('container');
     container.appendChild( renderer.domElement );
+
     createStream();
 
     video = document.getElementById( 'monitor' );
         
+    // webcam = importVideo('monitor', 0);
+
     videoImage = document.getElementById( 'videoImage0' );
     videoImageContext = videoImage.getContext( '2d' );
     videoImageContext.fillStyle = '#000000';
@@ -32,6 +35,8 @@ function init() {
     var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
     movieScreen.position.set(0,50,0);
     scene.add(movieScreen);
+
+    video.play();
                                             
     camera.position.set(0,150,300);
     camera.lookAt(movieScreen.position);
@@ -54,16 +59,25 @@ function render( time ){
    
     /* UPDATE VIDEO 
     */ 
-    for (var i = 0; i < video.length ; i++){
-        if ( video[i].readyState === video[i].HAVE_ENOUGH_DATA ) 
-        {
-            videoImageContext[i].drawImage( video[i], 0, 0, videoImage[i].width, videoImage[i].height );
-            if ( videoTexture[i] ) 
-                videoTexture[i].needsUpdate = true;
-        }
-        if( video[i].readyState === video[i].HAVE_ENOUGH_DATA ){
-          videoTexture[i].needsUpdate = true;
-        }
+    // for (var i = 0; i < video.length ; i++){
+    //     if ( video[i].readyState === video[i].HAVE_ENOUGH_DATA ) 
+    //     {
+    //         videoImageContext[i].drawImage( video[i], 0, 0, videoImage[i].width, videoImage[i].height );
+    //         if ( videoTexture[i] ) 
+    //             videoTexture[i].needsUpdate = true;
+    //     }
+    //     if( video[i].readyState === video[i].HAVE_ENOUGH_DATA ){
+    //       videoTexture[i].needsUpdate = true;
+    //     }
+    // }
+    if ( video.readyState === video.HAVE_ENOUGH_DATA ) 
+    {
+        videoImageContext.drawImage( video, 0, 0, videoImage.width, videoImage.height );
+        if ( videoTexture ) 
+            videoTexture.needsUpdate = true;
+    }
+    if( video.readyState === video.HAVE_ENOUGH_DATA ){
+      videoTexture.needsUpdate = true;
     }
     /* END VIDEO UPDATE
     */
